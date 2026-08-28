d0a778f315ac197d116c416a02ed2ec5b9e8c47e
Preet <kardapreet3@gmail.com>
2026-08-28 23:48:12 +0530

    Fix Render Flask deployment

--- app.py
+++ app.py
@@ -2038,11 +2038,10 @@ def return_book(issue_id):
 # =========================================================
 
 if __name__ == "__main__":
-
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 10000))
 
     app.run(
-        host="127.0.0.1",
+        host="0.0.0.0",
         port=port,
         debug=False
     )
