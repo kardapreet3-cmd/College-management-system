1596729845f08acc398af276c63ae6cbae7be3db
Preet <kardapreet3@gmail.com>
2026-08-28 21:51:57 +0530

    Fix Render database connection

--- app.py
+++ app.py
@@ -22,15 +22,14 @@ app.secret_key = "college_management_2026"
 # =========================================================
 # DATABASE CONNECTION
 # =========================================================
-
 import os
-
-db = mysql.connector.connect(
-    host=os.getenv("DB_HOST", "localhost"),
-    port=int(os.getenv("DB_PORT", "3306")),
-    user=os.getenv("DB_USER", "root"),
-    password=os.getenv("DB_PASSWORD", ""),
-    database=os.getenv("DB_NAME", "student_db")
+idb = mysql.connector.connect(
+    host=os.getenv("DB_HOST"),
+    port=int(os.getenv("DB_PORT", 14254)),
+    user=os.getenv("DB_USER"),
+    password=os.getenv("DB_PASSWORD"),
+    database=os.getenv("DB_NAME", "defaultdb"),
+    ssl_disabled=False
 )
 
 
