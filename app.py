cf4676d0bc3011901bd5e72d93d5abc88e19cce2
Preet <kardapreet3@gmail.com>
2026-08-29 15:35:08 +0530

    Fix database connection

--- app.py
+++ app.py
@@ -9,7 +9,15 @@ from flask import (
     url_for,
     session
 )
-
+def get_db():
+    return mysql.connector.connect(
+        host=os.environ.get("DB_HOST"),
+        port=int(os.environ.get("DB_PORT", 3306)),
+        user=os.environ.get("DB_USER"),
+        password=os.environ.get("DB_PASSWORD"),
+        database=os.environ.get("DB_NAME"),
+        ssl_disabled=False
+    )
 # =========================================================
 # FLASK APP
 # =========================================================
